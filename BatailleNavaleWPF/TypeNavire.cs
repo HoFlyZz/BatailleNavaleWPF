411b07fca223ed095f1870e8925d8d2768af7c1d
Jason <jay@hotmail.ca>
2022-09-15 14:58:18 -0400

    Ajoute du commentaire de ....

--- BatailleNavaleWPF/TypeNavire.cs
+++ BatailleNavaleWPF/TypeNavire.cs
@@ -1,2 +1,5 @@
 ﻿
 enum TypeNavire { SousMarin = 1, Destroyer, Patrouilleur, Cuirasse, PorteAvions }
+//bonjour monsieur le professeur nous allons travailler sur ce projet plus tard,
+//je ne vais pas mettre d'accent meme si j'écris en francais puis que les anglais me comprenderonsa pas, makes total fucking sens. English will follow...
+//I professor, here's the english version of my text to make sure im not using any non-english special characters. thanks you.
